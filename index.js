59062d0ecc5e6b1624eb8c420c9cc61bdade79f7
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2023-08-08 22:54:15 -0300

    feat: add AWS SQS integration with send and receive functionality

---
+++ index.js
@@ -0,0 +1,36 @@
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set the region 
+AWS.config.update({region: 'REGION'});
+
+// Create an SQS service object
+var sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+
+var params = {
+  MessageAttributes: {
+    "Title": {
+      DataType: "String",
+      StringValue: "The Whistler"
+    },
+    "Author": {
+      DataType: "String",
+      StringValue: "John Grisham"
+    },
+    "WeeksOn": {
+      DataType: "Number",
+      StringValue: "6"
+    }
+  },
+  MessageBody: JSON.stringify({ name: "rafael", content: "Olá bom dia, tudo bem ?\nGostaria de um apoio do seu time" }),
+  QueueUrl: "QUEUE_URL",
+  MessageGroupId: "Group1",
+  MessageDeduplicationId: new Date().getTime().toString(),
+};
+
+sqs.sendMessage(params, function(err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Success", data.MessageId);
+  }
+});
